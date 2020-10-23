6229408c71afeece5ae3df5fb16193153cf75dc1
lvyuqing <lvyuqing@thpower.com>
2020-10-23 18:16:23 +0800

    初始化avue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import Avue from '@smallwei/avue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import '@smallwei/avue/lib/index.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(Avue);
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
